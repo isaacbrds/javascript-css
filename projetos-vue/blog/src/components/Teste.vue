ddf916b772e3c5a0defe625d88143016e1b15c22
isaacbrds <isaacbrds>
2022-09-13 16:44:48 -0300

    Curso de VueJS - Vue cli

---
+++ projetos-vue/blog/src/components/Teste.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Meu componente</h1>
+    </div>
+</template>
+
+<script>
+
+export default ({
+    name: 'teste-componente'
+})
+</script>
+
+<style>
+  h1{
+    background: #333;
+    color: whitesmoke;
+  }
+</style>
