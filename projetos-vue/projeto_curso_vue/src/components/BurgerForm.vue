3b75925241453f09599318a598b4928ba003a33d
isaacbrds <isaacbrds>
2022-09-16 19:21:15 -0300

    Curso de VueJS - CRUD VUE

---
+++ projetos-vue/projeto_curso_vue/src/components/BurgerForm.vue
@@ -0,0 +1,109 @@
+<template>
+    <div>
+        <p>Componente de Mensagem</p>
+        <div>
+            <form id="burger-form">
+                <div class="input-container">
+                    <label for="nome">Nome do Cliente</label>
+                    <input type="text" id="nome" name="name" v-model="nome" placeholder="Digite o nome do cliente.">
+                </div>
+                
+                <div class="input-container">
+                    <label for="pao">Escolha o Pão</label>
+                    <select name="pao" id="pao" v-model="pao">
+                        <option value="">Selecione o pão</option>
+                        <option value="integral">Integral</option>
+                    </select>
+                </div>
+
+                <div class="input-container">
+                    <label for="carne">Escolha a Carne</label>
+                    <select name="carne" id="carne" v-model="carne">
+                        <option value="">Selecione o tipo de carne</option>
+                        <option value="maminha">Maminha</option>
+                    </select>
+                </div>
+
+                <div class="input-container" id="opcionais-container">
+                    <label for="opcionais" id="opcionais-title">Escolha os Opcionais</label>
+                    <div class="checkbox-container">
+                    
+                    <input type="checkbox" name="opcionais" id="opicionais" value="salame">
+
+                    <span>Salame</span>
+                    </div>
+                </div>
+                
+                <div class="input-container">
+                    <input type="submit" class="submit-btn" value="Criar meu Burguer!">
+                </div>
+            </form>
+        </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: "BurguerForm"
+}
+</script>
+
+<style scoped>
+  #burger-form {
+    max-width: 400px;
+    margin: 0 auto;
+  }
+  .input-container {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 20px;
+  }
+  label {
+    font-weight: bold;
+    margin-bottom: 15px;
+    color: #222;;
+    padding: 5px 10px;
+    border-left: 4px solid #fcba03;
+  }
+  input, select {
+    padding: 5px 10px;
+    width: 300px;
+  }
+  #opcionais-container {
+    flex-direction: row;
+    flex-wrap: wrap;
+  }
+  #opcionais-title {
+    width: 100%;
+  }
+  .checkbox-container {
+    display: flex;
+    align-items: flex-start;
+    width: 50%;
+    margin-bottom: 20px;
+  }
+  .checkbox-container span,
+  .checkbox-container input {
+    width: auto;
+  }
+  .checkbox-container span {
+    margin-left: 6px;
+    font-weight: bold;
+  }
+  .submit-btn {
+    background-color: #222;
+    color:#fcba03;
+    font-weight: bold;
+    border: 2px solid #222;
+    padding: 10px;
+    font-size: 16px;
+    margin: 0 auto;
+    cursor: pointer;
+    transition: .5s;
+  }
+  .submit-btn:hover {
+    background-color: transparent;
+    color: #222;
+  }
+</style>
