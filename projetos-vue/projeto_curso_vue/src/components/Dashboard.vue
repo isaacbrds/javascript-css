b495f2295c47c9b008cd57b0fa9b6175b448db5c
isaacbrds <isaac.brigido@gmail.com>
2022-09-17 18:06:32 -0300

    Curso de VUE - R do Crud dos pedidos

--- projetos-vue/projeto_curso_vue/src/components/Dashboard.vue
+++ projetos-vue/projeto_curso_vue/src/components/Dashboard.vue
@@ -10,15 +10,16 @@
     </div>
 
     <div id="burguer-table-rows">
-      <div class="burger-table-row">
-        <div class="order-number">1</div>
-        <div>Jão</div>
-        <div>Trigo</div>
-        <div>Maminha</div>
+      <div class="burger-table-row" v-for="burger in burgers" :key="burger.id">
+        <div class="order-number">{{burger.id}}</div>
+        <div>{{burger.nome}}</div>
+        <div>{{burger.pao}}</div>
+        <div>{{burger.carne}}</div>
         <div>
           <ul>
-            <li>Tomate</li>
-            <li>Salame</li>
+            <li v-for="(opcional,index) in burger.opcionais" :key="index">
+              {{opcional}}
+            </li>
           </ul>
         </div>
         <div>
@@ -35,7 +36,30 @@
 
 <script>
 export default {
-name: "Dashboard"
+  name: "Dashboard",
+  data(){
+    return {
+      burgers: null,
+      burgers_id:null,
+      status: []
+    }
+  },
+  methods: {
+    async getPedidos(){
+
+      const req = await fetch('http://localhost:3000/burgers');
+
+      const data = await req.json();
+
+      this.burgers = data;
+
+      console.log(this.burgers)
+    }
+  },
+  mounted(){
+    this.getPedidos();
+  }
+
 }
 </script>
 
