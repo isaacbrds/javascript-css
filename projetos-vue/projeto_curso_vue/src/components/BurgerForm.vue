e533696e7c68154106ff2228c0f5b3739153b595
isaacbrds <isaac.brigido@gmail.com>
2022-09-17 17:14:50 -0300

    Curso de VUE - Create do Crud com Vuejs

--- projetos-vue/projeto_curso_vue/src/components/BurgerForm.vue
+++ projetos-vue/projeto_curso_vue/src/components/BurgerForm.vue
@@ -2,7 +2,7 @@
     <div>
         <p>Componente de Mensagem</p>
         <div>
-            <form id="burger-form">
+            <form id="burger-form" @submit="createBurger">
                 <div class="input-container">
                     <label for="nome">Nome do Cliente</label>
                     <input type="text" id="nome" name="name" v-model="nome" placeholder="Digite o nome do cliente.">
@@ -55,7 +55,6 @@ export default {
         pao: null,
         carne: null,
         opcionais: [],
-        status: 'Solicitado',
         msg: null
       }
     },
@@ -67,6 +66,34 @@ export default {
         this.paes = data.paes
         this.carnes = data.carnes
         this.opcionaisData = data.opcionais
+      },
+
+      async createBurger(e){
+        e.preventDefault();
+        
+        const data = {
+          nome: this.nome,
+          carne: this.carne,
+          pao: this.pao,
+          opcionais: Array.from(this.opcionais),
+          status: "Solicitado",
+        }
+
+        const dataJson = JSON.stringify(data);
+
+        const req = await fetch('http://localhost:3000/burgers', {
+          method: "POST",
+          headers: {"Content-Type": "application/json"},
+          body: dataJson
+        });
+
+        const res = await req.json();
+        console.log(res);
+
+        this.nome = '';
+        this.carne = '';
+        this.pao = '';
+        this.opcionais = [];
       }
     },
     mounted(){
